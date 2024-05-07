23976bdd0f2a9430e5bb5104cfd9350ccf441d30
Mikhail Smirnov <smirnov.mi@gmail.com>
2024-05-07 22:23:59 +0200

    Ubuntu 24, removed public IPs, add private network. Load balancer doesn't work.

--- 1-infra/modules/hcloud/main.tf
+++ 1-infra/modules/hcloud/main.tf
@@ -7,15 +7,30 @@ resource "hcloud_server" "cloud_nodes" {
   for_each = var.nodes
 
   name        = each.value.name
-  image       = "ubuntu-20.04" 
+  image       = "ubuntu-24.04"
   server_type = each.value.server_type
   location    = var.hcloud_location
   ssh_keys    = ["access1"]
+  #user_data = file("${path.module}/cloud-config.yaml")
+  labels = {
+        "env" = "prod"
+  }
+  network {
+    network_id = data.hcloud_network.private_net.id
+        #network_id = data.hcloud_network.private_net.id
+        #ip = each.value.private_ip
+  }
+
+  public_net {
+    ipv4_enabled = false
+    ipv6_enabled = false
+  }
+
 }
 
 resource "hcloud_volume" "volumes" {
   for_each = var.nodes
-  
+
   name = "${each.value.name}-volx"
   size = 10
   server_id = hcloud_server.cloud_nodes[each.key].id
@@ -43,9 +58,10 @@ resource "hcloud_load_balancer" "load_balancer" {
   }
 }
 
+# SHOULD USE: hcloud_load_balancer_target
+
 resource "hcloud_load_balancer_network" "server_network_lb" {
   load_balancer_id = hcloud_load_balancer.load_balancer.id
   network_id = data.hcloud_network.private_net.id
   ip = var.load_balancer.private_ip
 }
-
